eeee3afb22e3856e05be0b905159562f238f7813
Ross Story <ross.story@dal.ca>
2016-01-05 17:23:00 -0800

    More analysis

--- script/analysis.py
+++ script/analysis.py
@@ -1,11 +1,31 @@
 #!/usr/bin/env python3
 
-import numpy
-import pandas
+import numpy as np
+import pandas as pd
 import lasagne
-import nolearn
+import nolearn as nl
 import os
+import scipy as sp
+import multiprocessing as mp
+import pickle
 
-for root, dirnames, filenames in os.walk('../imgs'):
-    print(dirnames)
-    print(filenames)
+sp.ndimage.imread
+
+def load_imgs(filenames):
+    data = np.zeros((len(filenames), 640, 640, 3))
+    for f in filenames:
+        results.append(pool.apply_async(sp.ndimage.imread, f))
+    for i, r in enumerate(results):
+        data[i,...] = results.get()
+    return data
+
+output = dict()
+pool = multiprocessing.Pool(multiprocessing.cpu_count())
+
+for root, _, filenames in os.walk('../imgs/Cities/'):
+    if len(filenames) > 0:
+        print(root)
+        output[root] = load_imgs(filenames, root)
+
+pickle.dump(output, "Output")
+
