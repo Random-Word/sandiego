9b50b4d5c9e021b0707c0d8093a1c918e223a60d
Ubuntu <ubuntu@ip-172-31-16-7.us-west-1.compute.internal>
2016-01-06 03:40:55 +0000

    Working on NNing

--- script/analysis.py
+++ script/analysis.py
@@ -6,26 +6,36 @@ import lasagne
 import nolearn as nl
 import os
 import scipy as sp
-import multiprocessing as mp
+import multiprocessing
 import pickle
 
-sp.ndimage.imread
+SELECTED_CITIES = ['Singapore', 'Sydney', 'Melbourne', 'Tokyo', 'Osaka',
+        'Paris', 'Madrid', 'Barcelona', 'Rome', 'Venice',
+        'San Francisco', 'New York', 'Stockholm', 'Johannesburg', 'Honolulu',
+        'Cape Town', 'Sao Paulo', 'Buenos Aires', 'Bangkok', 'Ankara',
+        'Seoul', 'Busan', 'Phnom Penh', 'Palermo', 'Athens', 'Vienna',
+        'Prague', 'Warsaw', 'Taipei']
 
-def load_imgs(filenames):
+
+def load_imgs(root, filenames):
     data = np.zeros((len(filenames), 640, 640, 3))
-    for f in filenames:
-        results.append(pool.apply_async(sp.ndimage.imread, f))
+    labels = np.zeros((len(filenames)))
+    results = list()
+    for i, f in enumerate(filenames):
+        labels[i] = SELECTED_CITIES.index(f.split('_')[0])
+        results.append(pool.apply_async(sp.ndimage.imread, [root+os.sep+f]))
     for i, r in enumerate(results):
-        data[i,...] = results.get()
-    return data
+        data[i,...] = r.get()
+    np.save("/mnt/data/"+root.split('/')[-1]+"_Data", data)
+    np.save("/mnt/data/"+root.split('/')[-1]+"_Labels", labels)
+    print(data.shape)
+    print(labels.shape)
 
-output = dict()
 pool = multiprocessing.Pool(multiprocessing.cpu_count())
 
 for root, _, filenames in os.walk('../imgs/Cities/'):
     if len(filenames) > 0:
         print(root)
-        output[root] = load_imgs(filenames, root)
+        load_imgs(root, filenames)
 
-pickle.dump(output, "Output")
 
